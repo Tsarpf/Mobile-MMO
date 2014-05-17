c23b960f8617134e877a5b91e45cc78191e47de6
unknown <essi.salmenkivi@helsinki.fi>
2014-05-18 01:32:49 +0300

    Added UnitySocket.io usage example stuff

--- Assets/Initializer.cs
+++ Assets/Initializer.cs
@@ -1,20 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
+using SocketIOClient; 
+using System;
 
 public class Initializer : MonoBehaviour {
 
 	public int SpaceDimensionX;
 	public int SpaceDimensionY;
+	Client client;
 	Floor floor;
 	// Use this for initialization
 	void Start () {
 		//SpaceDimensionX = 30; SpaceDimensionY = 20;
-
 		
+
+		client = new Client ("http://datisbox.net:3001");
+		client.Opened += SocketOpened;
+
+		client.Connect ();
+
         Material grid = Resources.Load("Materials/grid") as Material;
         Material floorMat = Resources.Load("Materials/grayfloor") as Material;
 
 		floor = new Floor(floorMat, grid, SpaceDimensionX, SpaceDimensionY);
+
+	}
+
+	void SocketOpened(object sender, EventArgs e)
+	{
+		client.Send ("Derp");
 	}
 	
 	// Update is called once per frame
